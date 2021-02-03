4b5ba2298b45723888805f0872c2ca1951f9d1af
Euna Park <parkeu@us.ibm.com>
2021-02-03 11:48:50 -0800

    Updated ServoControlCommand.java

--- src/main/java/frc/robot/commands/ServoControlCommand.java
+++ src/main/java/frc/robot/commands/ServoControlCommand.java
@@ -26,9 +26,10 @@ public class ServoControlCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Robot.robotContainer.getButtonPanel2().whenReleased(new RunServo(0, Robot.robotContainer.servoSubsystem));
-    Robot.robotContainer.getButtonPanel4().whenReleased(new RunServo(90, Robot.robotContainer.servoSubsystem));
-    Robot.robotContainer.getButtonPanel6().whenReleased(new RunServo(180, Robot.robotContainer.servoSubsystem));
+    Robot.robotContainer.getButtonA().whenReleased(new RunServo(0, Robot.robotContainer.servoSubsystem));
+    Robot.robotContainer.getButtonB().whenReleased(new RunServo(90, Robot.robotContainer.servoSubsystem));
+    Robot.robotContainer.getButtonX().whenReleased(new RunServo(180, Robot.robotContainer.servoSubsystem));
+    Robot.robotContainer.getButtonY().whenReleased(new RunServo(270, Robot.robotContainer.servoSubsystem));
   }
   
   // Called once the command ends or is interrupted.
