d35a79574fd0f79e1b935bed0be39c4689056614
Shannon Wang <shannonwang1029@gmail.com>
2021-02-15 17:10:14 -0800

    Fixed SPARK Max port, split shooter command into 2 to control motor power

--- src/main/java/frc/robot/subsystems/ShooterPrototypeSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterPrototypeSubsystem.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.ShooterConstants;
@@ -6,39 +7,39 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterPrototypeSubsystem extends SubsystemBase {
 
-  private CANSparkMax shooter;
-  private double targetPower;
+	private CANSparkMax shooter;
+	private double targetPower;
 
-  public ShooterPrototypeSubsystem() {
-    shooter = new CANSparkMax(ShooterConstants.SPARK_SHOOTER, MotorType.kBrushless);
-    targetPower = 0.3;
-    shooter.set(0); // on init, set power to 0
-  }
+	public ShooterPrototypeSubsystem() {
+		shooter = new CANSparkMax(ShooterConstants.SPARK_SHOOTER, MotorType.kBrushless);
+		targetPower = 0.3;
+		shooter.set(0); // on init, set power to 0
+	}
 
-  public void runShooterPower(double power) {
-    shooter.set(power);
-  }
+	public void runShooterPower(double power) {
+		shooter.set(power);
+	}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+	@Override
+	public void simulationPeriodic() {
+		// This method will be called once per scheduler run during simulation
+	}
 
-  public double getPower() {
-    return shooter.get();
-  }
+	public double getPower() {
+		return shooter.get();
+	}
 
-  public double getTargetPower() {
-    return targetPower;
-  }
+	public double getTargetPower() {
+		return targetPower;
+	}
 
-  public void setTargetPower(double targetPower) {
-    this.targetPower = targetPower;
-  }
+	public void setTargetPower(double targetPower) {
+		this.targetPower = targetPower;
+	}
 
 }
