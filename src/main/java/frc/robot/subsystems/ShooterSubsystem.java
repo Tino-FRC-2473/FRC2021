a41e027d6b6d170b21f0a8b47825426376476c4b
Shannon Wang <shannonwang1029@gmail.com>
2021-02-22 22:22:43 -0800

    added commands to start the shooter motor at teleop init, changed the intake subsystem variables to the correct variables

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,6 +6,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,6 +16,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
 	private CANSparkMax leftShooterMotor;
 	private CANSparkMax rightShooterMotor;
+	private double targetPower;
 
  	/** Creates a new ShooterSubsystem. */
 	public ShooterSubsystem() {
@@ -22,11 +24,20 @@ public class ShooterSubsystem extends SubsystemBase {
 		leftShooterMotor = new CANSparkMax(ShooterConstants.SPARK_SHOOTER_LEFT, MotorType.kBrushless);
 		rightShooterMotor = new CANSparkMax(ShooterConstants.SPARK_SHOOTER_RIGHT, MotorType.kBrushless);
 
+		targetPower = 0.1;
+		leftShooterMotor.set(0); // on init, set power to 0
+		rightShooterMotor.set(0);
+
 	}
 
 	public void runShooterPower(double power) {
 		leftShooterMotor.set(power);
 		rightShooterMotor.set(-power);
+	}
+
+	public void runShooterRPM(double rpm) { // 5834
+		leftShooterMotor.getPIDController().setReference(rpm, ControlType.kVelocity);
+		rightShooterMotor.getPIDController().setReference(-rpm, ControlType.kVelocity);
 	}
 
 	@Override
@@ -37,5 +48,17 @@ public class ShooterSubsystem extends SubsystemBase {
 	@Override
 	public void simulationPeriodic() {
     	// This method will be called once per scheduler run during simulation
+	}
+
+	public double getPower() {
+		return leftShooterMotor.get();
+	}
+
+	public double getTargetPower() {
+		return targetPower;
+	}
+
+	public void setTargetPower(double targetPower) {
+		this.targetPower = targetPower;
 	}
 }
