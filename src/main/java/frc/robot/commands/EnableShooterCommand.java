b38518e08c40d0b5077240d26f259a6d7bb4c40a
Shannon Wang <shannonwang1029@gmail.com>
2021-02-26 20:29:36 -0800

    have intake and shooter run at same time

--- src/main/java/frc/robot/commands/EnableShooterCommand.java
+++ src/main/java/frc/robot/commands/EnableShooterCommand.java
@@ -1,12 +1,14 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.IntakeStorageSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class EnableShooterCommand extends CommandBase {
 
 	private final ShooterSubsystem shooterSubsystem;
+	private final IntakeStorageSubsystem intakeStorageSubsystem;
 	private boolean status;
 
 	/**
@@ -14,8 +16,9 @@ public class EnableShooterCommand extends CommandBase {
 	 *
 	 * @param subsystem The subsystem used by this command.
 	 */
-	public EnableShooterCommand(ShooterSubsystem subsystem, boolean status) {
-		shooterSubsystem = subsystem;
+	public EnableShooterCommand(ShooterSubsystem shooterSubsystem, IntakeStorageSubsystem intakeStorageSubsystem, boolean status) {
+		this.shooterSubsystem = shooterSubsystem;
+		this.intakeStorageSubsystem = intakeStorageSubsystem;
 		this.status = status;
 	}
 
@@ -27,6 +30,8 @@ public class EnableShooterCommand extends CommandBase {
 		} else {
 			shooterSubsystem.runShooterPower(0);
 		}
+		intakeStorageSubsystem.runIntakeMotor(0.5);
+		intakeStorageSubsystem.runStorageMotor(0.5);
 	}
 
 	// Called every time the scheduler runs while the command is scheduled.
