68ce1cee347c4fd96dacc46624c7058f01e65a76
andrewxsimon <53956369+andrewxsimon@users.noreply.github.com>
2021-03-01 23:06:21 -0800

    Add gamepad drive
    Add basic teleop drive using gamepad 
    Co-authored-by: vkaranam <42920149+VaibhavKaranam@users.noreply.github.com>
    Co-authored-by: Andrewxsimon

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,43 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int TEST_PORT = 11;
+    public static final double ENCODER_INCHES_TO_TICKS = 1;
+
+    public static final int SPARK_FRONT_LEFT_ID = 3; 
+    public static final int SPARK_BACK_LEFT_ID = 4; 
+    public static final int SPARK_FRONT_RIGHT_ID = 1; 
+    public static final int SPARK_BACK_RIGHT_ID = 2;  
+
+    public static final double DRIVE_P = 5e-4;
+    public static final double DRIVE_I = 0;
+    public static final double DRIVE_D = 0;
+
+    public static final double DRIVE_TICKS_PER_INCH = 1;
+
+	public static final int WHEEL_PORT = 0;
+    public static final int LEFT_JOYSTICK_PORT = 1;
+    public static final int RIGHT_JOYSTICK_PORT = 2;
+    public static final int THROTTLE_PORT = 3;
+    public static final int BUTTON_PANEL_PORT = 4;
+    
+    
+    public static final int XBOX_CONTROLLER_PORT = 0;
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_X = 3;
+    public static final int BUTTON_Y = 4;
+
+    public static final int SERVO_PORT = 0;
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,44 +5,141 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.DriveSubsystem;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+	// The robot's subsystems and commands are defined here...
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+	public final DriveSubsystem driveSubsystem = new DriveSubsystem();
+	// public final ServoSubsystem servoSubsystem = new ServoSubsystem();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 * 
+	 */
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+	// Tank Drive
+	private XboxController driver;
+	private Joystick joystick;
+	private JoystickButton buttonA;
+	private JoystickButton buttonB;
+	private JoystickButton buttonX;
+	private JoystickButton buttonY;
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
+	// Arcade Drive
+	private Joystick wheel;
+	private Joystick throttle;
+	private Joystick buttonPanel;
+	public RobotContainer() {
+		// Configure the button bindings
+		configureButtonBindings();
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by instantiating a {@link GenericHID} or one of its subclasses
+	 * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+	 * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+		driver = new XboxController(Constants.XBOX_CONTROLLER_PORT);
+		// Tank Drive
+		joystick = new Joystick(Constants.LEFT_JOYSTICK_PORT);
+		buttonA = new JoystickButton(driver, Constants.BUTTON_A);
+		buttonB = new JoystickButton(driver, Constants.BUTTON_B);
+		buttonX = new JoystickButton(driver, Constants.BUTTON_X);
+		buttonY = new JoystickButton(driver, Constants.BUTTON_Y);
+
+		// Arcade Drive
+		wheel = new Joystick(Constants.WHEEL_PORT);
+		throttle = new Joystick(Constants.THROTTLE_PORT);
+		buttonPanel = new Joystick(Constants.BUTTON_PANEL_PORT);
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+		// An ExampleCommand will run in autonomous
+
+		return null; // no auto command yet
+	}
+
+	// public XboxController getDriver() {
+	// 	return driver;
+	// }
+
+	// public DriveSubsystem getDriveSystem() {
+	// 	return driveSubsystem;
+	// }
+
+	// public Joystick getLeftJoystick() {
+	// 	return leftJoystick;
+	// }
+
+	// public Joystick getRightJoystick() {
+	// 	return rightJoystick;
+	// }
+
+	public Joystick getWheel() {
+		return wheel;
+	}
+
+	public Joystick getThrottle() {
+		return throttle;
+	}
+
+	public Joystick getButtonPanel() {
+		return buttonPanel;
+	}
+
+	public JoystickButton getButtonA() {
+		return buttonA;
+	}
+
+	public JoystickButton getButtonB() {
+		return buttonB;
+	}
+
+	public JoystickButton getButtonX() {
+		return buttonX;
+	}
+
+	public JoystickButton getButtonY() {
+		return buttonY;
+	}
+
+	public double getLeftY() {
+		return driver.getY(GenericHID.Hand.kLeft);
+	}
+
+	public double getRightX() {
+		return driver.getX(GenericHID.Hand.kRight);
+	}
+
+	public double getRightY() {
+		return driver.getY(GenericHID.Hand.kRight);
+	}
+
+	public double getZ() {
+		return getThrottle().getZ();
+	}
+
+	public double getX() {
+		return getWheel().getX();
+	}
 }
