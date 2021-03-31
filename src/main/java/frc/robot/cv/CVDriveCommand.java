ae1bb3338f214f5cadbb5274230aa81a1d70b9a1
Rahul Bansal <rahulbansal2004@gmail.com>
2021-03-30 19:45:07 -0700

    changed starting position for blue paths

--- src/main/java/frc/robot/cv/CVDriveCommand.java
+++ src/main/java/frc/robot/cv/CVDriveCommand.java
@@ -44,24 +44,25 @@ public class CVDriveCommand extends SequentialCommandGroup {
         );
         BluePathA = 
         new SequentialCommandGroup (
-            new TurnUsingGyro(driveSubsystem, -21.8),
-            new StraightDrive(driveSubsystem, 161.6, 0.6),
+            new StraightDrive(driveSubsystem, 150.0, 0.6),
             new TurnUsingGyro(driveSubsystem, 71.6),
             new StraightDrive(driveSubsystem, 94.9, 0.6),
             new TurnUsingGyro(driveSubsystem, -26.6),
             new StraightDrive(driveSubsystem, 67.1, 0.6),
             new TurnUsingGyro(driveSubsystem, 0)
         );
+        //new TurnUsingGyro(driveSubsystem, -21.8),
+
         BluePathB = 
         new SequentialCommandGroup (
-            new TurnUsingGyro(driveSubsystem, -21.8),
-            new StraightDrive(driveSubsystem, 161.6, 0.6),
+            new StraightDrive(driveSubsystem, 120.0, 0.6),
             new TurnUsingGyro(driveSubsystem, 45),
             new StraightDrive(driveSubsystem, 84.9, 0.6),
             new TurnUsingGyro(driveSubsystem, -45),
             new StraightDrive(driveSubsystem, 84.9, 0.6),
             new TurnUsingGyro(driveSubsystem, 0)
         );
+        //new TurnUsingGyro(driveSubsystem, -21.8),
         
     }
 
